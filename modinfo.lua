c029510211a2c8fe555c0f9ecd8f832da742a103
genchsusu <pctxiaozhu@gmail.com>
2024-01-23 22:59:46 +0800

    feat: bug fix

--- modinfo.lua
+++ modinfo.lua
@@ -4,7 +4,7 @@ end
 
 name = e_or_z("Marine Engineering", "海上工程")
 author = 'OpenSource'
-version = '1.0.2'
+version = '1.0.3'
 description = e_or_z(
     [[
 Enhance your seafaring experience with customizable options.
@@ -54,25 +54,25 @@ local function AddConfig(label, name, hover, options, default)
 			{description = e_or_z("On", "开启"), data = true},
 			{description = e_or_z("Off", "关闭"), data = false},
 		},
-		default = default or true
+		default = default
 	}
 end
 
 configuration_options = 
 {
-    AddConfig(e_or_z('Button', '按钮'), 'boat_button', e_or_z("On or off button", "开启或者关闭按钮")),
+    AddConfig(e_or_z('Button', '按钮'), 'boat_button', e_or_z("On or off button", "开启或者关闭按钮"), nil, true),
     AddConfig(e_or_z('Anchor falling speed', '船锚升降速度'), 'anchor_speed', e_or_z("Adjust the speed of the anchor falling", "调整船锚升降速度"), {
         {description = e_or_z('Faster', '加快'), data = true},
         {description = e_or_z('Default', '默认'), data = false},
     }, true),
-    AddConfig(e_or_z('Anchor power', '船锚力量'), 'power_anchor', e_or_z("Enhance anchor anchoring capability", "强化船锚锚定能力")),
+    AddConfig(e_or_z('Anchor power', '船锚力量'), 'power_anchor', e_or_z("Enhance anchor anchoring capability", "强化船锚锚定能力"), nil, true),
     AddConfig(e_or_z('msat falling speed', '船帆升降速度'), 'mast_speed', e_or_z("Adjust the speed of the mast falling", "调整船帆升降速度"), {
         {description = e_or_z('Faster', '加快'), data = true},
         {description = e_or_z('Default', '默认'), data = false},
     }, true),
-    AddConfig(e_or_z('Mast speed', '船帆速度'), 'power_mast', e_or_z("Enhance mast propulsion capability", "强化船桅推进能力")),
-    AddConfig(e_or_z('Rudder speed', '船舵转速'), 'power_rudder', e_or_z("Enhance rudder control capability", "强化船舵操纵能力")),
-    AddConfig(e_or_z('Show fish', '显示鱼群'), 'show_fish', e_or_z("Show fish", "显示鱼群")),
-    AddConfig(e_or_z('Spawn fish', '召唤鱼群'), 'create_fish', e_or_z("Spawn fish near the ship", "在船旁边生成鱼群")),
-    AddConfig(e_or_z('No boat leak', '船永不沉没'), 'no_boat_leak', e_or_z("No boat leak", "船永不沉没")),
+    AddConfig(e_or_z('Mast speed', '船帆速度'), 'power_mast', e_or_z("Enhance mast propulsion capability", "强化船桅推进能力"), nil, true),
+    AddConfig(e_or_z('Rudder speed', '船舵转速'), 'power_rudder', e_or_z("Enhance rudder control capability", "强化船舵操纵能力"), nil, true),
+    AddConfig(e_or_z('Show fish', '显示鱼群'), 'show_fish', e_or_z("Show fish", "显示鱼群"), nil, true),
+    AddConfig(e_or_z('Spawn fish', '召唤鱼群'), 'create_fish', e_or_z("Spawn fish near the ship", "在船旁边生成鱼群"), nil, true),
+    AddConfig(e_or_z('No boat leak', '船永不沉没'), 'no_boat_leak', e_or_z("No boat leak", "船永不沉没"), nil, true),
 }
